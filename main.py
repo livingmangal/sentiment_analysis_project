7942b7475fd21fc05c44b3df4c012fa90ca0030e
Mangal Singh <livingmangalsingh.02@gmail.com>
2025-04-02 00:11:08 +0530

    requirements3

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from app.api import app
 import os
 
+# This is needed for gunicorn to find the app
+application = app
+
 if __name__ == '__main__':
+    # This is only used when running directly with Python
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
