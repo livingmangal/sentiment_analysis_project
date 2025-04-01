9438f46924214527e655824a510d878ba279514d
Mangal Singh <livingmangalsingh.02@gmail.com>
2025-04-01 23:26:34 +0530

    RENDER REQUIREMENTS

--- app/api.py
+++ app/api.py
@@ -111,4 +111,4 @@ def predict() -> tuple[Dict[str, Any], int]:
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
